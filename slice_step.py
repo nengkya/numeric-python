ce291c5ce871f92e44c813484cbfbcde1dd7d2a0
nengkya <nengkya@yahoo.com>
2023-09-10 00:30:39 +0700

    Slice negative backward array[1, 1, -3:-1]

--- slice_step.py
+++ slice_step.py
@@ -1,6 +1,9 @@
 import numpy as np
 
-array = np.array([1, 83, 74, 25, 13])
+array = np.array([1, 83, 74, 25, 13, 44, 17, 55, 66, 77, 88, 99, 13, 10])
 
+print()
 print(array, '\n')
-print('array[1:5:2] is ', array[1:5:2])
+print('array[1:5:2] is ', array[1:5:2], '\n')
+
+print('array[::3]   is ', array[::3], '\n')

45b53b0165a91783f04144c795b45f16cce18bb2
nengkya <nengkya@yahoo.com>
2023-09-09 11:47:04 +0700

    print('Last element from 3rd dimension is ', array[2, 1, -2])

--- numpy_array.py
+++ numpy_array.py
@@ -3,7 +3,7 @@ import numpy
 array = numpy.array(
     [
         [[1, 2, 3, 4, 5], [11, 22, 33, 44, 55]],
-        [[1, 1, 1, 1, 1], [ 2,  2,  2,  3,  3]],
+        [[1, 1, 1, 1, 1], [ 2, 12,  2,  3,  3]],
         [[3, 3, 3, 3, 3], [ 3,  3,  3,  3,  4]]
     ])
 
@@ -15,3 +15,5 @@ array2 = numpy.array([1, 2, 33, 4, 5], ndmin = 5)
 print(array2)
 
 print("2nd element on 1st row is ", array[1, 1, 1])
+
+print("Last element from 3rd dimension is ", array[2, 1, -2])

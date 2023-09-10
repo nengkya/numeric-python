0ea7793bb06e6c671de673778fac671d22bd5b5f
nengkya <nengkya@yahoo.com>
2023-09-10 18:52:24 +0700

    iterate 3d to 2d

--- reshape_3d.py
+++ reshape_3d.py
@@ -3,4 +3,8 @@ import numpy as np
 array = np.array([17, 22, 36, 47, 53, 65, 57, 82, 94, 10, 12, 11])
 
 array = array.reshape(3, 2, 2)
-print(array)
+
+for iterate_3d in array:
+    for iterate_2d in iterate_3d:
+        print('------------')
+        print(iterate_2d)

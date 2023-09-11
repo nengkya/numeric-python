ce2348f7ba2c5062cd52c27f7b4b4e581769d530
nengkya <nengkya@yahoo.com>
2023-09-11 20:27:29 +0700

    numpy.concatenate((array1, array2))

---
+++ numpy_concatenate.py
@@ -0,0 +1,9 @@
+import numpy
+
+array1 = numpy.arange(36).reshape(3, 4, 3)
+array2 = numpy.random.randint(44, size=(3, 4, 3))
+#print(array1)
+#print(array2)
+
+array = numpy.concatenate((array1, array2), axis = 0)
+print(array)
